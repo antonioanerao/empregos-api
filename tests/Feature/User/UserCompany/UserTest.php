ffe6ce9c401b7a6da98bea51c4089ec0a7a71d19
Antonio Anerão <anerao.junior@gmail.com>
2022-06-20 05:39:30 -0500

    Refatorando UserTest melhorando o test de company can post a job

--- tests/Feature/User/UserCompany/UserTest.php
+++ tests/Feature/User/UserCompany/UserTest.php
@@ -4,11 +4,9 @@ namespace Tests\Feature\User\UserCompany;
 
 use App\Models\Job;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Feature\User\UserData;
 use Tests\TestCase;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class UserTest extends TestCase
 {
@@ -22,29 +20,13 @@ class UserTest extends TestCase
     }
 
     public function test_a_company_can_post_a_job() {
-        $this->withExceptionHandling();
-
         $this->post(UserData::registerUrl(), array_merge(UserData::data(), ['type' => 2]));
         $this->assertCount(1, User::all());
         $user = User::first();
 
-        $headers = [
-            'Authorization' => 'Bearer ' . JWTAuth::fromUser($user)
-        ];
-
-        $response = $this->withHeaders($headers)->post('api/jobs', [
-            'user_id' => $user->id,
-            'title' => 'my new job',
-            'companyName' => 'Company',
-            'description' => nl2br('My job description'),
-            'email' => 'mail@domain.com',
-            'phone' => 'my phone',
-            'expirationDate' => Carbon::now()->addDays(7),
-            'status' => 1
-        ]);
+        $response = $this->withHeaders(UserData::headers($user))->post('api/jobs', UserData::newJobData($user->id));
 
         $this->assertCount(1, Job::all());
-
         $response->assertStatus(201);
     }
 }
--- tests/Feature/User/UserData.php
+++ tests/Feature/User/UserData.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\User;
 
+use Carbon\Carbon;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
 class UserData
 {
     public static function data() {
@@ -20,5 +23,24 @@ class UserData
 
     public static function loginUrl() {
         return "/api/auth/login";
+    }
+
+    public static function headers($user) {
+        return [
+            'Authorization' => 'Bearer ' . JWTAuth::fromUser($user)
+        ];
+    }
+
+    public static function newJobData($user) {
+        return [
+            'user_id' => $user,
+            'title' => 'my new job',
+            'companyName' => 'Company',
+            'description' => nl2br('My job description'),
+            'email' => 'mail@domain.com',
+            'phone' => 'my phone',
+            'expirationDate' => Carbon::now()->addDays(7),
+            'status' => 1
+        ];
     }
 }
